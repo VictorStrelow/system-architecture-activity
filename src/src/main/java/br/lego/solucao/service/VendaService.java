ea031441c478891fb637a3a07fca82e459c25ff3
victor_strelow <victorstrelow53@gmail.com>
2026-02-13 20:29:53 -0300

    feat: Code for VendaService

--- src/src/main/java/br/lego/solucao/service/VendaService.java
+++ src/src/main/java/br/lego/solucao/service/VendaService.java
@@ -1,5 +1,33 @@
 package br.lego.solucao.service;
 
+import br.lego.solucao.domain.model.LegoSet;
+import br.lego.solucao.domain.ports.repositories.PedidoRepository;
+import br.lego.solucao.domain.ports.strategies.FreteStrategy;
+
 public class VendaService {
+
+    private final FreteStrategy freteStrategy;
+    private final PedidoRepository pedidoRepository;
+
+    public VendaService(FreteStrategy freteStrategy, PedidoRepository pedidoRepository) {
+        this.freteStrategy = freteStrategy;
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public void registrarCompra(LegoSet lego) {
+        System.out.println("\n--- PROCESSANDO VENDA ---");
+
+        double valorFrete = freteStrategy.calcular(lego.getPeso());
+        double total = lego.getPreco() + valorFrete;
+
+        System.out.println("Carro: " + lego.getNome());
+        System.out.println("Preço Base: R$" + lego.getPreco());
+        System.out.println("Frete (" + freteStrategy.getNomeMetodo() + "): R$" + valorFrete);
+        System.out.println("TOTAL A PAGAR: R$" + total  );
+
+        pedidoRepository.salvarVenda(lego, freteStrategy.getNomeMetodo(), valorFrete, total);
+
+        System.out.println("--- VENDA FINALIZADA ---");
+    }
 
 }
